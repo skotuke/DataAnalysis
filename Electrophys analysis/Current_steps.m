f8ef687cf3b106c76e428c12d9c19d336031fd7a
skotuke <skotuke@gmail.com>
2015-05-14 14:41:22 +0100

    Completely new structure in current steps

--- Electrophys analysis/Current_steps.m
+++ Electrophys analysis/Current_steps.m
@@ -1,22 +1,29 @@
 close all
-[filenames, path] = uigetfile({'*.abf'}, 'Select_file(s)', 'MultiSelect', 'on');
 
 path = fileparts(mfilename('fullpath'));
+delete(sprintf('%s/Output/Current_steps/*.xlsx', path));
 addpath(sprintf('%s/Includes', path));
 
-if iscell(filenames)
-    number_of_files = length(filenames);
-    for i = 1:number_of_files
+[filenames, path] = uigetfile({'*.abf'}, 'Select_file(s)', 'MultiSelect', 'on');
+
+if ~iscell(filenames) %if filenames is not an array
+    filenames = {filenames};%make it into one element array. we want it in am array because you cannot have text inthe matrix
+end
+
+number_of_files = length(filenames); %length is a function getting a number 
+m = 1;
+
+for i = 1:number_of_files
         fullname = strcat(path, filenames(i));
         data = abfload(fullname{1});
         name = filenames(i);
-        CurrentStepsFunction(data, i - 1, name{1});
+   
+    if isempty(data)
+        continue
     end
-elseif filenames == 0    
-    % Don't do anything
-else
-    fullname = strcat(path, filenames);
-    data = abfload(fullname);
-    CurrentStepsFunction(data, 1, filenames);
+    
+      CurrentStepsFunction(data, fullname, 'Current_Steps',m);
+      m=m+1;
+      
 end
 
